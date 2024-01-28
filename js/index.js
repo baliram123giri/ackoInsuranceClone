7a81dbde0e39f695d81d51e0709a7aab7c1ffe5d
baliram123giri <giri710401@gmail.com>
2024-01-28 08:47:47 +0530

    acko done

--- js/index.js
+++ js/index.js
@@ -105,3 +105,159 @@ for (let i = 0; i < videoList.length; i++) {
     })
 }
 
+//creating array of objects
+const ackoData = [{
+    img: "/ackoCard1.svg",
+    title: "Incredibly low premiums",
+    description: "Insuring things that matter to you shouldn’t come at a cost. That’s why when you buy from us, you get insurance that fits your pocket."
+},
+{
+    img: "/ackoCard2.svg",
+    title: "Superquick and easy",
+    description: "We’re easy, effortless and 100% digital. Forget paperwork, just access all our services with just a few clicks."
+},
+{
+    img: "/ackoCard3.svg",
+    title: "Hassle-free claims",
+    description: "We’re on a mission to make insurance claims stress-free. Just give us a call or file a claim online and experience cashless, speedy claim settlements."
+},
+]
+
+//acko section
+const ackoSection = document.getElementById("acko_section")
+
+for (let i = 0; i < ackoData.length; i++) {
+    //created div element
+    const ackoCard = document.createElement("div")
+    ackoCard.className = "col-12 col-lg-4"
+    //adding attributes
+    // ackoCard.setAttribute("class", "col-12 col-lg-4")
+    // ackoCard.style.background = i % 2 == 0 ? "red" : "green"
+    // ackoCard.style.color = i % 2 == 0 ? "white" : "black"
+
+    //create img tag
+    const img = document.createElement("img")
+    //add src attributes
+    img.src = "../images" + ackoData[i].img
+    img.alt = "dynamic image"
+    img.width = 120
+
+    //create h4 tag
+    const h4 = document.createElement("h4")
+    h4.innerText = ackoData[i].title
+
+    //create p tag
+    const p = document.createElement("p")
+    p.innerText = ackoData[i].description
+
+
+    //adding child of ackoCard
+    ackoCard.appendChild(img)
+    ackoCard.appendChild(h4)
+    ackoCard.appendChild(p)
+
+    //adding child of ackoSection
+    ackoSection.appendChild(ackoCard)
+}
+
+
+const footerNavData = [
+    {
+        title: "Products",
+        navLinks: ["Motor Insurance", "Car Insurance", "Bike Insurance", "Health Insurance", "Travel Insurance", "International Travel Insurance", "Life Insurance", "Term Insurance", "Group Health Insurance"]
+    },
+    {
+        title: "Company",
+        navLinks: ["About us", "Board of directors", "Bike Insurance", "Health Insurance", "Travel Insurance", "International Travel Insurance", "Life Insurance", "Term Insurance", "Group Health Insurance"]
+    },
+    {
+        title: "Company",
+        navLinks: ["About us", "Board of directors", "Bike Insurance", "Health Insurance", "Travel Insurance", "International Travel Insurance", "Life Insurance", "Term Insurance", "Group Health Insurance"]
+    },
+    {
+        title: "Company",
+        navLinks: ["About us", "Board of directors", "Bike Insurance", "Health Insurance", "Travel Insurance", "International Travel Insurance", "Life Insurance", "Term Insurance", "Group Health Insurance"]
+    },
+    {
+        title: "Company",
+        navLinks: ["About us", "Board of directors", "Bike Insurance", "Health Insurance", "Travel Insurance", "International Travel Insurance", "Life Insurance", "Term Insurance", "Group Health Insurance"]
+    },
+    {
+        title: "Company",
+        navLinks: ["About us", "Board of directors", "Bike Insurance", "Health Insurance", "Travel Insurance", "International Travel Insurance", "Life Insurance", "Term Insurance", "Group Health Insurance"]
+    },
+    {
+        title: "Company",
+        navLinks: ["About us", "Board of directors", "Bike Insurance", "Health Insurance", "Travel Insurance", "International Travel Insurance", "Life Insurance", "Term Insurance", "Group Health Insurance"]
+    },
+    {
+        title: "Company",
+        navLinks: ["About us", "Board of directors", "Bike Insurance", "Health Insurance", "Travel Insurance", "International Travel Insurance", "Life Insurance", "Term Insurance", "Group Health Insurance"]
+    },
+    {
+        title: "Company",
+        navLinks: ["About us", "Board of directors", "Bike Insurance", "Health Insurance", "Travel Insurance", "International Travel Insurance", "Life Insurance", "Term Insurance", "Group Health Insurance"]
+    },
+    {
+        title: "Company",
+        navLinks: ["About us", "Board of directors", "Bike Insurance", "Health Insurance", "Travel Insurance", "International Travel Insurance", "Life Insurance", "Term Insurance", "Group Health Insurance"]
+    },
+
+]
+
+//footer navbar 
+const footerNavbar = document.getElementById("footer_navbar")
+
+for (let i = 0; i < footerNavData.length; i++) {
+
+    //create div and give class names col-12 col-lg-3
+    const div = document.createElement("div")
+    //there are 3 ways to add class with value
+    // div.classList.add("col-12", "col-lg-3")
+    // div.setAttribute("class", "col-12 col-lg-3")
+    div.className = "col-12 col-lg-3 mt-4"
+
+    //crerte h5
+    const h5 = document.createElement("h5")
+    
+    //1)innerText
+    //2)innerHTML
+    h5.innerText = footerNavData[i].title
+    //create ul element
+    const ul = document.createElement("ul")
+    ul.style.listStyle = "none"
+
+    //loops start on the footerNavData[i].navLinks
+    for (let link = 0; link < footerNavData[i].navLinks.length; link++) {
+        const li = document.createElement("li")
+        const a = document.createElement("a")
+        a.style.textDecoration = "none"
+        a.style.color = "#000"
+        a.classList.add("a_force_fontsize")
+        a.href = "#"
+        a.innerText = footerNavData[i].navLinks[link]
+        //adding li to ul
+        ul.appendChild(li)
+        //adding a to li
+        li.appendChild(a)
+    }
+
+    //going inside the div
+    div.appendChild(h5)
+    //adding ul
+    div.appendChild(ul)
+
+    //append the childs
+    footerNavbar.appendChild(div)
+
+}
+
+
+
+
+
+
+
+
+
+
